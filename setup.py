15a570f461c407bd16a9143f1ef577376be225c4
abdul rab <rababdul5786@gmail.com>
2025-06-18 12:53:37 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Build Chrome Plugin that extracts comments of youtube video and predicts sentiment of comments and does analysis on comments.',
+    author='Abdul Rab',
+    license='',
+)
